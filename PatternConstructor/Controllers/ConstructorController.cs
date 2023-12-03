fc1ab4646eb7af5e8d54ae605cc2bbea21073c18
Blocknotick <130940436+Blocknotick@users.noreply.github.com>
2023-12-03 05:21:30 +0400

    MVP or smth

--- PatternConstructor/Controllers/ConstructorController.cs
+++ PatternConstructor/Controllers/ConstructorController.cs
@@ -1,17 +1,26 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿//using iText.Kernel.Pdf;
+using iTextSharp.text.pdf;
+using iText.Html2pdf;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using PatternConstructor.Data;
 using PatternConstructor.Models;
 using PatternConstructor.ViewModels;
+using iText.Kernel.Pdf;
+using iText.Kernel.Geom;
+using iText.Svg.Processors;
+using iText.Svg.Processors.Impl;
+using iText.Svg.Converter;
 //using Aspose.Svg;
 //using Aspose.Svg.Saving;
 //using Aspose.Imaging;
-using Aspose.Svg.Rendering.Pdf;
-using Aspose.Svg.Rendering;
+////using Aspose.Svg.Rendering.Pdf;
+////using Aspose.Svg.Rendering;
 
-//using Aspose.Html;
+////using Aspose.Html;
+////using Aspose.Html.Drawing;
 //using Aspose.Html.Dom.Svg;
 //using Aspose.Html.Converters;
 //using Aspose.Html.Saving;
@@ -93,49 +102,46 @@ namespace PatternConstructor.Controllers
             
 
             string dataDir = "wwwroot/sunskirts/";
-            //SVGDocument document1 = new SVGDocument(dataDir + "test.svg");
-            //SVGDocument document2 = new SVGDocument(dataDir + "test1.svg");
-            //SVGDocument document3 = new SVGDocument(dataDir + "Lineto.svg");
 
             // Create an instance of SvgRenderer
-            SvgRenderer renderer = new SvgRenderer();
+            //SvgRenderer renderer = new SvgRenderer();
 
-            // Specify PdfRenderingOptions
-            var options = new PdfRenderingOptions()
-            {
-                // Set Page Setup properties
-                PageSetup =
-                {
-                    Sizing = SizingType.FitContent
-                }
-            };
-
-
+            //// Specify PdfRenderingOptions
+            //var options = new PdfRenderingOptions()
+            //{
+            //    // Set Page Setup properties
+            //    PageSetup =
+            //    {
+            //        Sizing = SizingType.FitContent
+            //    }
+            //};
 
 
-            var user = await _userManager.GetUserAsync(User);
-            var createdFile = new CreatedFile
-            {
-                UserId = user.Id,
-            };
-            await _context.AddAsync(createdFile);
-            _context.SaveChanges();
 
-            string FileName = user.Id + createdFile.Id.ToString()+".pdf";
 
-            // Create an instance of PdfDevice
-            PdfDevice device = new PdfDevice(options, dataDir + FileName);
+            //var user = await _userManager.GetUserAsync(User);
+            //var createdFile = new CreatedFile
+            //{
+            //    UserId = user.Id,
+            //};
+            //await _context.AddAsync(createdFile);
+            //_context.SaveChanges();
 
-            // Merge or combine all SVG documents to a PDF file.
-            renderer.Render(device, document, document);
+            //string FileName = user.Id + createdFile.Id.ToString()+".pdf";
 
-            createdFile.PatternLink = "/sunskirts/" + FileName;
-            createdFile.Name = sunSkirtConstructModel.Name;
-            _context.Update(createdFile);
-            _context.SaveChanges();
+            //// Create an instance of PdfDevice
+            //PdfDevice device = new PdfDevice(options, dataDir + FileName);
+
+            //// Merge or combine all SVG documents to a PDF file.
+            //renderer.Render(device, document, document);
+
+            //createdFile.PatternLink = "/sunskirts/" + FileName;
+            //createdFile.Name = sunSkirtConstructModel.Name;
+            //_context.Update(createdFile);
+            //_context.SaveChanges();
 
             //переход к странице созданных файлов
-            return RedirectToAction("Index");
+            return RedirectToAction("History", "Account");
         }
 
         [Authorize]
@@ -206,11 +212,43 @@ namespace PatternConstructor.Controllers
             //Здесь должна быть функция, которая генерирует выкройку
             string documentContent = "<svg xmlns=\"http://www.w3.org/2000/svg\"><circle cx=\"50\" cy=\"50\" r=\"40\" /></svg>";
             // Initialize an object of SVGDocument class from the string content
-            Aspose.Svg.SVGDocument document = new Aspose.Svg.SVGDocument(documentContent, ".");
+            //Aspose.Svg.SVGDocument document = new Aspose.Svg.SVGDocument(documentContent, ".");
 
-            ConvertPatternToPDF(new Aspose.Svg.SVGDocument[] {document, document}, dataDir + FileName);
+
+            using (iText.Kernel.Pdf.PdfDocument doc =
+                new iText.Kernel.Pdf.PdfDocument(new iText.Kernel.Pdf.PdfWriter(new FileStream("wwwroot/skirts/" + FileName, FileMode.OpenOrCreate),
+                                                                                new WriterProperties().SetCompressionLevel(0))))
+            {
+                doc.AddNewPage(PageSize.A4);
+                //ISvgConverterProperties properties = new SvgConverterProperties().SetBaseUri(SVG_FILE);
+                SvgConverter.DrawOnDocument(documentContent, doc, 1);
+            }
+
+
+            //ConvertPatternToPDF(new Aspose.Svg.SVGDocument[] {document, document}, dataDir + FileName);
             //логика генерации описания пошива
-            GenerateDescription(new string[] { "wwwroot/DescriptionUnits/interfacing.html", "wwwroot/DescriptionUnits/zipper.html" }, "wwwroot/descr/" + FileName);
+
+            var htmldocs = new List<string>();
+            //if (skirtConstructModel.SkirtCombinationModel.Type == "Прямая" || skirtConstructModel.SkirtCombinationModel.Type == "Тюльпан")
+            //    htmldocs.Add("wwwroot/DescriptionUnits/dart.html");
+            //htmldocs.Add("wwwroot/DescriptionUnits/interfacing.html");
+            //htmldocs.Add("wwwroot/DescriptionUnits/belt.html");
+
+            //htmldocs.Add("wwwroot/DescriptionUnits/sideseam.html");
+            //htmldocs.Add("wwwroot/DescriptionUnits/bottomseam.html");
+            //if (skirtConstructModel.SkirtCombinationModel.Clasp == "Потайная молния")
+            //    htmldocs.Add("wwwroot/DescriptionUnits/zipper.html");
+            //else
+            //    htmldocs.Add("wwwroot/DescriptionUnits/button.html");
+
+            htmldocs.Add("wwwroot/DescriptionUnits/bottomseam.pdf");
+            if (skirtConstructModel.SkirtCombinationModel.Clasp == "Потайная молния")
+                htmldocs.Add("wwwroot/DescriptionUnits/zipper.pdf");
+            else
+                htmldocs.Add("wwwroot/DescriptionUnits/button.pdf");
+
+            createPdf("wwwroot/descr/" + FileName, htmldocs);
+            //GenerateDescription(htmldocs, "wwwroot/descr/" + FileName);
             createdFile.PatternLink = "/skirts/" + FileName;
             createdFile.DescribtionLink = "/descr/" + FileName;
             createdFile.Name = skirtConstructModel.Name;
@@ -221,42 +259,53 @@ namespace PatternConstructor.Controllers
             return RedirectToAction("History","Account");
         }
 
-
-        void GenerateDescription(string[] htmldocs, string outputpath)
-        {
-            //изначально объявить арреем, Боже!
-            var doc = new List<Aspose.Html.HTMLDocument>();
-            for (int i = 0; i < htmldocs.Length; i++)
+        public void createPdf(string file, List<string> HTML){
+            iTextSharp.text.Document document = new iTextSharp.text.Document();
+            PdfCopy copy = new PdfCopy(document, new FileStream(file, FileMode.Create, FileAccess.Write));
+            document.Open();
+            iTextSharp.text.pdf.PdfReader reader;
+            foreach (var html in HTML)
             {
-                doc.Add(new Aspose.Html.HTMLDocument(htmldocs[i]));
+                reader = new iTextSharp.text.pdf.PdfReader(html);
+                copy.AddDocument(reader);
+                reader.Close();
             }
-            //var document1 = new Aspose.Html.HTMLDocument("wwwroot/test.html");
-
-            //поля бы добавить
-            var renderer1 = new Aspose.Html.Rendering.HtmlRenderer();
-            //renderer1.Render(new Aspose.Html.Rendering.Pdf.PdfDevice(dataDir + "output.pdf"), document1, document1);
-            //renderer1.Render(new Aspose.Html.Rendering.Pdf.PdfDevice(outputpath), document1, document1);
-            renderer1.Render(new Aspose.Html.Rendering.Pdf.PdfDevice(outputpath),doc.ToArray());
-        }
-
-        void ConvertPatternToPDF(Aspose.Svg.SVGDocument[] svgfiles, string outputpath)
-        {
-            
-
-            // Create an instance of SvgRenderer
-            SvgRenderer renderer = new SvgRenderer();
-
-            // Specify PdfRenderingOptions
-            var options = new PdfRenderingOptions();
-
-            
-
-            // Create an instance of PdfDevice
-            PdfDevice device = new PdfDevice(options, outputpath);
-
-            // Merge or combine all SVG documents to a PDF file.
-            renderer.Render(device, svgfiles);
+            document.Close();
 
         }
+        //void GenerateDescription(List<string> htmldocs, string outputpath)
+        //{
+        //    var doc = new List<Aspose.Html.HTMLDocument>();
+        //    //Aspose.Html.HTMLDocument [] doc = new Aspose.Html.HTMLDocument[0];
+        //    for (int i = 0; i < htmldocs.Count; i++)
+        //    {
+        //        doc.Add(new Aspose.Html.HTMLDocument(htmldocs[i]));
+        //    }
+        //    //var document1 = new Aspose.Html.HTMLDocument("wwwroot/test.html");
+
+        //    //поля бы добавить
+        //    var renderer1 = new Aspose.Html.Rendering.HtmlRenderer();
+        //    //renderer1.Render(new Aspose.Html.Rendering.Pdf.PdfDevice(dataDir + "output.pdf"), document1, document1);
+        //    //renderer1.Render(new Aspose.Html.Rendering.Pdf.PdfDevice(outputpath), document1, document1);
+        //    //var options = new Aspose.Html.Rendering.Pdf.PdfRenderingOptions();
+        //    //options.PageSetup.AnyPage = new Page(new Margin(10));
+        //    renderer1.Render(new Aspose.Html.Rendering.Pdf.PdfDevice(outputpath),doc.ToArray());
+        //}
+
+        //void ConvertPatternToPDF(Aspose.Svg.SVGDocument[] svgfiles, string outputpath)
+        //{
+        //    // Create an instance of SvgRenderer
+        //    SvgRenderer renderer = new SvgRenderer();
+
+        //    // Specify PdfRenderingOptions
+        //    var options = new PdfRenderingOptions();
+
+        //    // Create an instance of PdfDevice
+        //    PdfDevice device = new PdfDevice(options, outputpath);
+
+        //    // Merge or combine all SVG documents to a PDF file.
+        //    renderer.Render(device, svgfiles);
+
+        //}
     }
 }
